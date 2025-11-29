edfb674c7db3c287d6217ceff360970c4a1673f5
byborh <byborh@gmail.com>
2025-11-29 22:12:38 +0100

    Implement Mandelbrot set generation with proper complex arithmetic | Fix Pixmap struct: use Pixel* with dynamic allocation

--- main.c
+++ main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <complexe.h>
+#include <complex.h>
 
-// TODO: make it dinamic !
-#define LENGTH 900 // 10 - 600 - 900
-#define HEIGHT 400 // 10 - 600 - 600
+// Constantes
+#define LENGTH 900
+#define HEIGHT 600
 #define RAYON 100
 #define OPACITY 255
 #define IMG_FILE "image.ppm"
 #define ZERO 0
-#define MANDELBROT = 1
+#define X1 -2.0
+#define Y1 1.0
+#define X2 1.0
+#define Y2 -1.0
 
-// Define all Structures
+// Structures
 typedef struct 
 {
     unsigned int r;
@@ -20,109 +23,114 @@ typedef struct
     unsigned int b;
 } Pixel;
 
-
 typedef struct
 {
     char signature[3];
     int length;
     int height;
-    Pixel pixels;
+    Pixel* pixels;
 } Pixmap;
 
-//  Define functions
+// Déclarations des fonctions
 int createImage(Pixmap pixmap, int generation);
 int createSquare(Pixmap pixmap);
 int createCircle(Pixmap pixmap);
 int createMandelbrot(Pixmap pixmap);
-// TODO à créer !
-void tryCatch(){};
-void verifyCreationOfImage(){};
-
+int convergence(double x, double y);
 
 int main() {
     printf("Ce fichier est lancé correctement !\n");
 
     Pixel pixels;
     pixels.r = 0;
-    pixels.g = 123;
+    pixels.g = 255;
     pixels.b = 255;
 
     Pixmap pixmap;
-    // pixmap.signature = "P6";
     strcpy(pixmap.signature, "P3");
-    pixmap.length = LENGTH;
+    pixmap. length = LENGTH;
     pixmap.height = HEIGHT;
-    pixmap.pixels = pixels;
+    pixmap.pixels = (Pixel*)malloc(LENGTH * HEIGHT * sizeof(Pixel));
+    
+    if (!pixmap. pixels) {
+        printf("Erreur allocation mémoire !\n");
+        return 1;
+    }
 
-    int res = createImage(pixmap, 1);
-    if(res == 1) printf("Le fichier est crée ! Vas tchéquer ça gros !\n");
-    else printf("Le fichier n'est pas crée... C'est toi le problème, pas le code :(\n");
+    for (int i = 0; i < LENGTH * HEIGHT; i++) {
+        pixmap.pixels[i] = pixels;
+    }
 
+    int res = createImage(pixmap, 3);
+    if (res == 1) printf("Le fichier est crée !  Vas tchéquer ça gros !\n");
+    else printf("Le fichier n'est pas crée...  C'est toi le problème, pas le code :(\n");
+
+    free(pixmap.pixels);
     return 0;
 }
 
-
 /*
-    You need to send a generation number to create a different type of image.
-    Exemples :
-        0 - Create a square with a uni color
-        1 - Create a sqaure with a circle in the middle
-        2 - 
-
+    0 - Create a square with a uni color
+    1 - Create a square with a circle in the middle
+    3 - Create Mandelbrot set
 */
 int createImage(Pixmap pixmap, int generation) {
     
-    if(generation == 0) createSquare(pixmap);
+    if (generation == 0) createSquare(pixmap);
     else if (generation == 1) createCircle(pixmap);
-    else if(generation == 3) createMandelbrot(pixmap);
+    else if (generation == 3) createMandelbrot(pixmap);
     
-    printf("Allez vérifier si j'ai pu crée le fichier ou pas, normalement, si ! sinon, c TOI le problème BHAHAHAHAHAH\n");
+    printf("Allez vérifier si j'ai pu crée le fichier ou pas, normalement, si !  sinon, c TOI le problème BHAHAHAHAHAH\n");
 
     return 1;
-};
+}
 
+/*
+    Create a square with a uni color
+*/
 int createSquare(Pixmap pixmap) {
     FILE* f = fopen(IMG_FILE, "w");
-    if(!f) {
+    if (!f) {
         printf("Le fichier est non ouvrable/modifiable !\n");
         return 0;
     }
 
-
     fprintf(f, "%s\n%d %d\n%d\n", pixmap.signature, pixmap.length, pixmap.height, OPACITY);
-    for(int j = 0; j < HEIGHT; j++) {
-        for(int i = 0; i < LENGTH; i++) {
-            fprintf(f, "%d %d %d ", pixmap.pixels.r, pixmap.pixels.g, pixmap.pixels.b);
-            printf(" %d %d %d ", pixmap.pixels.r, pixmap.pixels.g, pixmap.pixels.b);
+    for (int j = 0; j < HEIGHT; j++) {
+        for (int i = 0; i < LENGTH; i++) {
+            int index = j * LENGTH + i;
+            fprintf(f, "%d %d %d ", pixmap.pixels[index].r, pixmap.pixels[index].g, pixmap.pixels[index]. b);
         }
         fprintf(f, "\n");
-        printf("\n");
     }
 
-
-
     fclose(f);
+    return 1;
 }
 
+/*
+    Create a square with a circle in the middle
+*/
 int createCircle(Pixmap pixmap) {
-    int cx = HEIGHT/2;
-    int cy = LENGTH/2;
-
+    int cx = LENGTH / 2;
+    int cy = HEIGHT / 2;
     int radius = RAYON * RAYON;
 
     FILE* f = fopen(IMG_FILE, "w");
-    if(!f) {
+    if (!f) {
         printf("Le fichier est non ouvrable/modifiable !\n");
         return 0;
     }
 
     fprintf(f, "%s\n%d %d\n%d\n", pixmap.signature, pixmap.length, pixmap.height, OPACITY);
-    for(int x = 0; x < HEIGHT; x++) {
-        for(int y = 0; y < LENGTH; y++) {
-            int dx =  x - cx;
-            int dy =  y - cy;
-            if((dx*dx) + (dy*dy) <= radius) {
-                fprintf(f, "%d %d %d ", pixmap.pixels.r, pixmap.pixels.g, pixmap.pixels.b);
+    for (int y = 0; y < HEIGHT; y++) {
+        for (int x = 0; x < LENGTH; x++) {
+            int dx = x - cx;
+            int dy = y - cy;
+            int index = y * LENGTH + x;
+            
+            if ((dx * dx) + (dy * dy) <= radius) {
+                fprintf(f, "%d %d %d ", pixmap.pixels[index].r, pixmap.pixels[index].g, pixmap.pixels[index]. b);
             } else {
                 fprintf(f, "%d %d %d ", ZERO, ZERO, ZERO);
             }
@@ -130,26 +138,36 @@ int createCircle(Pixmap pixmap) {
         fprintf(f, "\n");
     }
     
-
     fclose(f);
+    return 1;
 }
 
+/*
+    Create Mandelbrot set
+*/
 int createMandelbrot(Pixmap pixmap) {
     FILE* f = fopen(IMG_FILE, "w");
-    if(!f) {
+    if (! f) {
         printf("Le fichier est non ouvrable/modifiable !\n");
         return 0;
     }
 
     fprintf(f, "%s\n%d %d\n%d\n", pixmap.signature, pixmap.length, pixmap.height, OPACITY);
-    for(int j = 0; j < HEIGHT; j++) {
-        for(int i = 0; i < LENGTH; i++) {
-            fprintf(f, "%d %d %d ", pixmap.pixels.r, pixmap.pixels.g, pixmap.pixels.b);
+    for (int j = 0; j < HEIGHT; j++) {
+        for (int i = 0; i < LENGTH; i++) {
+            double x = X1 + (X2 - X1) * i / (LENGTH - 1);
+            double y = Y1 + (Y2 - Y1) * j / (HEIGHT - 1);
+
+            int c = convergence(x, y);
+
+            unsigned char g = (3 * c > 255) ? 255 : 3 * c;
+            fprintf(f, "%d %d %d ", 0, g, 255);
         }
         fprintf(f, "\n");
     }
 
     fclose(f);
+    return 1;
 }
 
 int convergence(double x, double y) {
@@ -158,10 +176,7 @@ int convergence(double x, double y) {
     int max_iterations = 85;
     
     for (int n = 0; n < max_iterations; n++) {
-        if (cabs(u) >= 2. 0) {
-            return n + 1;
-        }
-        
+        if (cabs(u) >= 2.0) return n + 1;
         u = u * u + z;
     }
     
