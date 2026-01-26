2b6936841894450d531a461ef69fff325cb12b2d
byborh <byborh@gmail.com>
2026-01-26 15:22:24 +0100

    bubble sort

--- sort.c
+++ sort.c
@@ -4,17 +4,32 @@ int main() {
     // Sort by insertion !
     int tab[6] = {561, 465, 0, 21, 354, 3};
     int n = sizeof(tab) / sizeof(tab[0]);
-    for(int i=0; i<n; i++) {
-        int index_min = i;
-        for(int j=i+1; j<n; j++) {
-            if(tab[j] < tab[index_min]) {
-                index_min = j;
+
+    // printf("Sort by insertion: \n");
+    // for(int i=0; i<n; i++) {
+    //     int index_min = i;
+    //     for(int j=i+1; j<n; j++) {
+    //         if(tab[j] < tab[index_min]) {
+    //             index_min = j;
+    //         }
+    //     }
+    //     // tab[i], tab[index_min] = tab[index_min], tab[i];
+    //     int tmp = tab[i];
+    //     tab[i] = tab[index_min];
+    //     tab[index_min] = tmp;
+    // }
+
+    
+    // Bubble sort
+    printf("Sort by insertion: \n");
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<n-i-1; j++) {
+            if(tab[j] > tab[j+1]) {
+                int tmp = tab[j];
+                tab[j] = tab[j+1];
+                tab[j+1] = tmp;
             }
         }
-        // tab[i], tab[index_min] = tab[index_min], tab[i];
-        int tmp = tab[i];
-        tab[i] = tab[index_min];
-        tab[index_min] = tmp;
     }
 
     printf("Tableau trié: ");
